fa7f58dd7ba337d25a8fcda4e38e636b34d09702
Animesh Arya <arya07@Animeshs-MacBook-Air.local>
2024-07-23 22:13:49 +0530

    Updated in Accordian.js

--- src/Accordion.js
+++ src/Accordion.js
@@ -8,7 +8,7 @@ export function Accordion({data}) {
 function AccordianItem({num,title, text}) {
   const [isOpen, setIsOpen] = useState(false);
   return(
-    <div className="item" onClick={()=> setIsOpen(!isOpen)}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={()=> setIsOpen(!isOpen)}>
       <p className="number">{num < 9 ? `0${num+1}` : num+1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
